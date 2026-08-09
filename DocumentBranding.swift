ca7869db9fca3c29eb0bfcffd31af7bb1f32af21
Bassam Faraj (Jr) <bassam@phoenixhovan.com>
2026-08-08 22:45:30 -0400

    Initial Commit

---
+++ DocumentBranding.swift
@@ -0,0 +1,39 @@
+public enum DocumentBranding {
+    public enum BrandStyle {
+        public let name: String
+        public let accentHex: String
+        public let contacts: [String]
+
+        public init(name: String, accentHex: String, contacts: [String]) {
+            self.name = name
+            self.accentHex = accentHex
+            self.contacts = contacts
+        }
+    }
+
+    public static let defaultBrand = BrandStyle(
+        name: "Default",
+        accentHex: "#FF0000",
+        contacts: ["default@example.com"]
+    )
+}
+
+public enum BrandPalette {
+    /// Phoenix Hovan default brand style with contacts.
+    public static let phoenixHovan = BrandStyle(
+        name: "Phoenix Hovan",
+        accentHex: "#0B5FFF",
+        contacts: [
+            "bassam@phoenixhovan.com",
+            "business@phoenixhovan.com",
+            "+1-614-615-3444"
+        ]
+    )
+}
+
+private extension String {
+    func isValidHexColor() -> Bool {
+        let regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+        return self.range(of: regex, options: .regularExpression) != nil
+    }
+}
